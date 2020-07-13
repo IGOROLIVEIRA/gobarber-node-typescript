bdaf7118d9540dfbee485f9e553b5b295c3343c4
Igor <ruas.igor@gmail.com>
2020-07-13 14:48:45 -0300

    Finalizando

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -3,6 +3,7 @@ import { injectable, inject } from 'tsyringe';
 import User from '@modules/users/infra/typeorm/entities/User';
 import ICacheProvider from '@shared/container/providers/CachProvider/models/ICacheProvider';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import { classToClass } from 'class-transformer';
 
 interface IRequest {
   user_id: string;
@@ -18,16 +19,17 @@ class ListProvidersService {
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    let users = await this.cacheProvider.recover<User[]>(
-      `providers-list:${user_id}`,
-    );
+    let users = null;
 
     if (!users) {
       users = await this.usersRepository.findAllProviders({
         except_user_id: user_id,
       });
 
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(
+        `providers-list:${user_id}`,
+        classToClass(users),
+      );
     }
 
     return users;
